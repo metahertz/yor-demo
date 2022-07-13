433dab57043c856b8f8b53e0c5da0aa1b79c7f2c
Matt Johnson <matt@bridgecrew.io>
2022-07-13 17:30:54 +0100

    S3 Bucket Added

--- terraform/aws_s3_bucket.tf
+++ terraform/aws_s3_bucket.tf
@@ -1,12 +1,8 @@
-resource "aws_s3_bucket" "name" {
-  tags = {
-    git_commit           = "0e067cc9804c7f955a658cef97730ff649375a4e"
-    git_file             = "terraform/aws_s3_bucket.tf"
-    git_last_modified_at = "2022-07-07 09:54:54"
-    git_last_modified_by = "James.Woolfenden@gmail.com"
-    git_modifiers        = "James.Woolfenden"
-    git_org              = "metahertz"
-    git_repo             = "yor-demo"
-    yor_trace            = "694a5c0c-5ff7-4907-8ae3-36dda0f578b3"
-  }
+resource "aws_s3_bucket" "holeinmy" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
 }
